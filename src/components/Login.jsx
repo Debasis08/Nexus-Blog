027ebf9b28450f7ce6406bf96b2a495a90713e8e
CH. DEBASIS PATRO <91054408+Debasis08@users.noreply.github.com>
2024-04-02 17:54:29 +0530

    Updated Logo section of Login Component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -30,9 +30,8 @@ function Login() {
     <div className='flex items-center justify-center m-7'>
       <div className='mx-auto rounded-2xl w-full max-w-lg bg-theme-300 p-10 border-2 border-theme-300'>
       <div className="mb-2 flex justify-center">
-                    <span className="inline-block w-full max-w-[150px]">
-                        <Logo width="100%" />
-                    </span>
+                    <h className="gradientText text-3xl font-serif md:text-3xl font-semibold xl:text-5xl"
+                                ><Link to='/'> NEXUS BLOG</Link></h>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
         <p className="mt-2 text-center text-base text-black/60">
