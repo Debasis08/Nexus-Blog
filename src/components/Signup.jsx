25ab88f923579efa6220c0dba86bf98b06b46265
CH. DEBASIS PATRO <91054408+Debasis08@users.noreply.github.com>
2024-04-02 17:55:33 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -31,9 +31,8 @@ function Signup() {
     <div className="flex items-center justify-center">
             <div className={`mx-auto w-full max-w-lg bg-theme-300 rounded-xl p-10`}>
             <div className="mb-2 flex justify-center">
-                    <span className="inline-block w-full max-w-[100px]">
-                        <Logo width="100%" />
-                    </span>
+                    <h className="gradientText text-3xl font-serif md:text-3xl font-semibold xl:text-5xl"
+                                ><Link to='/'> NEXUS BLOG</Link></h>
                 </div>
                 <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
                 <p className="mt-2 text-center text-base text-black/60">
