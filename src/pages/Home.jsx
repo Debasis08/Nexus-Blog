53299167281fd139c51a635e5eaaba9e8c21be56
CH. DEBASIS PATRO <91054408+Debasis08@users.noreply.github.com>
2024-04-11 03:39:52 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,8 @@ export default function Home() {
                 <Container>
                         <div className=" my-auto text-cente pt-11 md:pt-24 xl:pt-40 h-full  ">
                             <h1 className="text-xl font-mono md:text-4xl text-theme-400 font-bold hover:text-indigo-950">
-                                Kindly Login to Read Posts
+                                Kindly Login to Read Posts <br/>
+                                (Still in Progress)
                             </h1>
                         </div>
                 </Container>
