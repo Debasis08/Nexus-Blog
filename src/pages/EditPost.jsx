b8338a0edc68aeb9f956860e5bebe4015a6f1dd7
CH. DEBASIS PATRO <91054408+Debasis08@users.noreply.github.com>
2024-04-18 05:22:14 +0530

    Added Toast package to EditPost.jsx

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react'
 import { Container, PostForm } from '../components'
 import appwriteService from "../appwrite/configure"
 import { useNavigate, useParams } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function EditPost() {
     const [post, setPosts] = useState(null)
@@ -21,6 +23,7 @@ function EditPost() {
     }, [slug, navigate])
   return post ? (
     <div className='py-8'>
+     <ToastContainer/>
       <Container>
         <PostForm post = {post} />
       </Container>
