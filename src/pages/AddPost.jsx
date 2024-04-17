dea80cb1a8c7a8596146cab8a294d51522588056
CH. DEBASIS PATRO <91054408+Debasis08@users.noreply.github.com>
2024-04-18 05:23:28 +0530

    Update AddPost.jsx

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import {Container, PostForm} from '../components'
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function AddPost() {
   return (
     <div className='py-8'>
+     <ToastContainer/>
       {/* <Container> */}
         <PostForm />
       {/* </Container> */}
