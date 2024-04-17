7aa363d6eb9454541bcbb149624f36180f5d2fe9
CH. DEBASIS PATRO <91054408+Debasis08@users.noreply.github.com>
2024-04-18 04:58:15 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -47,6 +47,10 @@ function Login() {
                         Sign Up
                     </Link>
         </p>
+
+                <ToastContainer/>
+
+          
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
         <form onSubmit={handleSubmit(login)} className='mt-8'>
             <div className='space-y-5'>
