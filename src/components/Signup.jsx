68661134b502d144921b2f2718ecf0bc397f8ee0
CH. DEBASIS PATRO <91054408+Debasis08@users.noreply.github.com>
2024-04-19 23:07:14 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -33,22 +33,22 @@ function Signup() {
 
   return (
     <div className="flex items-center justify-center">
-            <div className={`mx-auto w-full max-w-lg bg-theme-300 rounded-xl p-10`}>
+            <div className={`mx-auto w-full max-w-lg bg-theme-300 rounded-xl p-10 border-2 border-sky`}>
             <div className="mb-2 flex justify-center">
                     <h className="gradientText text-3xl font-serif md:text-3xl font-semibold xl:text-5xl"
                                 ><Link to='/'> NEXUS BLOG</Link></h>
                 </div>
-                <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
-                <p className="mt-2 text-center text-base text-black/60">
+                <h2 className="text-center text-theme-100 text-2xl font-bold leading-tight">Sign up to create account</h2>
+                <p className="mt-2 text-theme-100 text-center text-base">
                     Already have an account?&nbsp;
                     <Link
                         to="/login"
-                        className="font-medium text-primary transition-all duration-200 hover:underline"
+                        className="font-medium text-primary transition-all duration-200 hover:underline focus:text-sky"
                     >
                         Sign In
                     </Link>
                 </p>
-                {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+                {error && <p className="text-red mt-8 text-center">{error}</p>}
 
                 <ToastContainer/>
 
@@ -80,7 +80,7 @@ function Signup() {
                         {...register("password", {
                             required: true,})}
                         />
-                        <Button type="submit" className="w-full bg-white hover:bg-theme-400 hover:text-white border-theme-400 border-2">
+                        <Button type="submit" className="w-full bg-white hover:bg-sky hover:text-white border-sky border-4">
                             <strong>Create Account</strong>
                         </Button>
 
