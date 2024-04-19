8b55ad3fdcc41114ad9c5dc8bc06a4071ccaba7c
CH. DEBASIS PATRO <91054408+Debasis08@users.noreply.github.com>
2024-04-19 21:12:03 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -37,28 +37,21 @@ function Header() {
   ]
 
   return (
-    <header className='xl:h-32 lg:h-20 md:h-28 h-24 z-40 bg-theme-300 border-b-4 border-white text-theme-400 rounded-lg sticky top-0 font-bold'>
+    <header className='xl:h-32 lg:h-20 md:h-28 h-24 z-40 pr-4 md:pr-7 bg-theme-300 text-white rounded-t-lg sticky top-0 font-semibold shadow-md shadow-theme-200'>
       {/* <Container> */}
         <nav className='flex'>
-{/*           <div className='lg:flex lg:h-14 lg:w-96 lg:mt-3 md:w-32 md:mt-5 md:ml-16 md:my-auto mr-4 ml-2 flex-none w-28 h-22'>
-          <div className='lg:w-1/4 md:w-32 w-36'>
-          <Link to='/'>
-            <Logo width='20px' />
-
-          </Link>
-          </div> */}
-         <div className='flex xl:h-32 lg:h-20 md:h-28 h-24 md:w-2/5 lg:w-3/6 justify-center items-center text-3xl md:text-3xl xl:text-7xl font-serif text-center lg:justify-center lg:items-center w-3/5'>
-            <h className="gradientText text-3xl md:text-3xl xl:text-5xl"
+         <div className='flex xl:h-32 lg:h-20 md:h-28 h-24 md:w-2/5 lg:w-3/6 text-3xl md:text-3xl xl:text-7xl text-center justify-center items-center w-3/5'>
+            <h className="gradientText text-xl md:text-2xl xl:text-5xl"
             ><Link to='/'> NEXUS BLOG</Link></h>
           </div>
           {/* </div> */}
-          <ul className= 'lg:pl-10 pr-1 xl:pl-52 md:w-2/5 w-3/5 ml-auto flex justify-end items-center'>
+          <ul className= 'lg:pl-10 pr-1 xl:pl-52 md:w-2/5 w-4/5 ml-auto flex justify-end items-center'>
             {navItems.map((item) =>
             item.active ? (
               <li key={item.name}>
                 <button
                 onClick={() => navigate(item.slug)}
-                className=' lg:w-28 md:text-lg md:w-28 my-auto md:mr-2 text-sm px-2 py-2 duration-200 bg-theme-300 hover:bg-gradient-to-tl from-theme-300 to-theme-200 hover:text-white hover:text-base md:hover:text-xl rounded-2xl focus:underline outline-none'>
+                className=' lg:w-28 md:text-lg md:w-28 my-auto mr-1 md:mr-4 text-sm px-2 py-2 duration-200 hover:bg-theme-400 md:bg-theme-400 md:hover:bg-theme-200 hover:text-white hover:text-base md:hover:text-xl rounded-2xl focus:outline outline-sky'>
                   {item.name}
                 </button>
               </li>
