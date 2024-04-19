5c0277c89ae38cfdabe1e1b2275d7d53d12ce449
CH. DEBASIS PATRO <91054408+Debasis08@users.noreply.github.com>
2024-04-20 01:12:04 +0530

    Update Spinner.jsx

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import loader from "./loader.gif"
 
 function Spinner() {
   return (
-    <div className='flex justify-center text-center w-screen h-4/5'>
+    <div className='flex justify-center scale-80 text-center w-screen h-4/5'>
       <img src={loader} alt='loading...' />
     </div>
   )
