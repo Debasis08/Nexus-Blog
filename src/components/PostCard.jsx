83b7f51bdb6d0ef708a7731819cfbf73df2e921c
CH. DEBASIS PATRO <91054408+Debasis08@users.noreply.github.com>
2024-04-22 13:54:05 +0530

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -8,7 +8,7 @@ function PostCard({$id, title,featuredImage}) {
     <Link to = {`/post/${$id}`}>
       <div className='w-full bg-theme-200 box-border rounded-xl shadow-inner shadow-sky p-2'>
         <div className='w-full flex justify-center mb-4'>
-            <img src={appwriteService.getFilePreview(featuredImage)} alt= {title} className='rounded-xl' />
+            <img src={appwriteService.getFilePreview(featuredImage)} alt= {title} title={title} className='rounded-xl' />
         </div>
         <h2 className='text-base md:text-2xl text-center text-white font-medium p-1'>{title}</h2>
       </div>
