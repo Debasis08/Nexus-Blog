5d92d9bc8f681bf9882b68689f2312bc9935e56d
CH. DEBASIS PATRO <91054408+Debasis08@users.noreply.github.com>
2024-04-26 18:40:28 +0530

    Update AllPosts.jsx

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -23,20 +23,8 @@ export default function AllPosts() {
     const cardsColumn1 = posts.slice(0, midIndex);
     const cardsColumn2 = posts.slice(midIndex);
 
-    if (posts.length===0) {
-      return (loader ? <Spinner/> :
-          <div className='flex flex-col md:pt-20 lg:pt-40 pt-10 text-center text-white '>
-            <div className='text-2xl font-bold top-0 text-white hover:text-opacity-90'>
-            Hey ! I think, there are no Blogs posted yet...
-            </div>
-            <div className='text-sm font-normal text-white '>
-            How about you post one ! 😊
-            </div>
-          </div>
-      )
-  } else {
-  return (
-    <div className='w-full lg:p-10 flex justify-center md:pl-56 p-8 overflow-auto bg-theme-400'>
+    return (loader ? <Spinner/> :
+    <div className='w-full lg:p-10 flex justify-center p-8 pt-24 lg:pt-32 overflow-auto bg-theme-400'>
         <Container>
                 <div className='grid w-1/2 pr-2 h-full xl:pr-6 gap-3 xl:gap-10 md:gap-5 grid-cols-1'>
                     {cardsColumn1.reverse().map((post) => (
