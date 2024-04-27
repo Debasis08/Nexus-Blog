488b8c403b60894bdd83be771dfd27b192826b43
CH. DEBASIS PATRO <91054408+Debasis08@users.noreply.github.com>
2024-04-27 09:16:07 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import Spinner from '../components/Spinner'
 import mypic from '../../image/mypic.png'
 
 
 export default function Home() {
     const [loader, setLoader] = useState(true)
+    const authStatus = useSelector((state) => state.auth.status)
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -24,7 +26,8 @@ export default function Home() {
                         </div>
              
                         <div className='text-white shadow-inner lg:scale-110 lg:pb-3 shadow-sky border-sky border-4 p-3 text-sm md:text-lg lg:text-2xl description w-3/5 h-96'>
-                           <span className='block text-center pt-7'> Welcome to <span className='gradientText2 font-semibold text-2xl lg:text-4xl lg:font-extrabold'>Nexus</span>, the Blog.</span><br/> You can use me to check blogs of people around the world.<br/> You can post blog on your cherished moments of trips or your beloved food item experience.<br/><br/><br/> <span className=' text-theme-100 block text-center'>Feel Free to <Link to="./signup" className='hover:text-sky underline font-semibold'>Sign-Up</Link> and Upload your first Blog <br/><br/> You can also <Link to="login" className='hover:text-sky underline font-semibold'>login</Link> to your account and check posts.</span>
+                           <span className='block text-center pt-7'> Welcome to <span className='gradientText2 font-semibold text-2xl lg:text-4xl lg:font-extrabold'>Nexus</span>, the Blog.</span><br/> You can use it to check blogs of people around the world.<br/> You can post blogs to share your cherished moments of trips or your favorite food item experience.<br/><br/><br/> {!authStatus && (<span className=' text-theme-100 block text-center'>Feel Free to <Link to="./signup" className='hover:text-sky underline font-semibold'>Sign-Up</Link> and Upload your first Blog <br/><br/> You can also <Link to="login" className='hover:text-sky underline font-semibold'>login</Link> to your account and check posts.</span>)}
+                            {authStatus && (<span className=' text-theme-100 block text-center'>Now, You can add posts and let the world know about your adventure 🥳 !!)}
                         </div>
                     </div>
                 </div>
